74304e0e70bdaff770946b01bda30b82a6c621b9
Leonard wegmann simon <leonard.wegmannsimon@elev.ga.ntig.se>
2024-03-19 10:02:07 +0100

    startat pirre projekt

---
+++ SlutProjekt/Program.cs
@@ -0,0 +1,25 @@
+﻿using Raylib_cs;
+
+Raylib.InitWindow(800,900,"Pizza simulator");
+Raylib.SetTargetFPS(61);
+
+string scene = "start";
+
+while(!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+
+    if (scene == "start")
+    {
+        Raylib.ClearBackground(Color.Gray);
+
+        Raylib.DrawText("*OBJECTIVE",60,210,35,Color.Black);
+        Raylib.DrawText("Shoot the dubious pizzas",60,260,35,Color.Black);
+        Raylib.DrawText("Be careful to not shoot the tasty ones*",60,310,35,Color.Black);
+        Raylib.DrawText("PRESS ENTER TO START",60,500,50,Color.Black);
+
+    }
+
+
+    Raylib.EndDrawing();
+}
